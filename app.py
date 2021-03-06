59a4cfb1b73b615dbbbf84011de56bd8e1dcc9f1
Vanessa Tang <vmtang11@gmail.com>
2021-03-06 22:09:47 +0000

    cash on cash calculator

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-#!/usr/bin/env python
 import click
 
 @click.command()
-def hello():
-    click.echo('Hello World! Welcome to my docker project')
-
+@click.option('--pp', type = float)
+@click.option('--hoa', type = float)
+@click.option('--tax', type = float)
+@click.option('--ins', type = float)
+@click.option('--rent', type = float)
+def cash_on_cash(pp, hoa, tax, ins, rent):
+    '''
+    Calculate cash on cash
+    Inputs: pp - purchase price
+            hoa - monthly hoa fee
+            tax - monthly property tax
+            ins - monthly home insurance
+            rent - monthly rent
+    Output: cash on cash in percent
+    '''
+    dep = 27.5
+    yr_dep = pp/dep
+    yr_tax = tax * 12
+    yr_hoa = hoa * 12
+    yr_ins = ins * 12
+    expenses = yr_tax + yr_hoa + yr_ins
+    
+    yr_rent = rent * 12
+    
+    btcf = yr_rent - expenses
+    cash_on_cash = btcf/pp * 100
+    
+    click.echo('Your estimated cash on cash is {0:.2f}%.'.format(cash_on_cash))
+    
 if __name__ == '__main__':
-    hello()
+    cash_on_cash()
